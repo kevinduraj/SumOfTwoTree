ef0db6c324a4e0f068d91c39d63eceadb5240848
Kevin Duraj <kevinduraj@gmail.com>
2014-11-05 14:55:50 -0800

    update to handle negative numbers

--- src/sumoftwotree/SumOfTwoTree.java
+++ src/sumoftwotree/SumOfTwoTree.java
@@ -9,31 +9,33 @@ public class SumOfTwoTree {
     /*--------------------------------------------------------------------------------------------*/
     public static void main(String[] args) {
 
-        int val = 5;
-
+        int twoSum = 44;
+        int[] array = { -2, -14, -3, 5, 6, 7, 10, 34, 3, 56, 100 };
+        
         Set<Integer> set = new TreeSet<>();
-
-        for (int i = 0; i < 30; i++) {
-            set.add((int) (Math.random() * 30));
-        }
-
-        System.out.println("Given Data = " + set);
+        
+        for (int i = 0; i < array.length; i++) {
+            set.add(array[i]);
+        }            
+        System.out.println("Given Data:\n" + set);
+        System.out.println("Searching for sum of two: " + twoSum + "\n");
 
         Iterator<Integer> iter1 = set.iterator();
         while (iter1.hasNext()) {
             
             int a = iter1.next();
-            if (a > val) break;
-
 
             Iterator<Integer> iter2 = set.iterator();
             while (iter2.hasNext()) {
 
                 int b = iter2.next();
-                if (b > val) break;
 
-                if ((a + b) == val) {
+                if ((a + b) == twoSum) {
                     System.out.printf("%2d + %2d = %2d\n", a, b, a + b);
+                } else if ((a + b) > twoSum) {
+                    //int sum = a + b;
+                    //System.out.println("breaking: " + a + " + " + b + " = " + sum);
+                    break;
                 }
             }
         }
